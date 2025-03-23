fd923b498a69523791580bda2a4192003abf24fb
Wally <wschageman@proton.me>
2025-03-23 17:38:59 -0600

    initial commit

---
+++ main.c
@@ -0,0 +1,10 @@
+int int main(int argc, char *argv[]) {
+  // Load configuration files if any.
+
+  // Run command loop.
+  lsh_loop();
+
+  // Perform any shutdown/cleanup.
+
+  return EXIT_SUCCESS;
+}
